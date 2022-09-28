022a205beed7db7bce36cf04995992dc8fb0fb86
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-09-28 22:39:16 +0600

    added activitis into home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Activities from '../Activities/Activities';
 import "./Home.css"
 
 const Home = () => {
   return (
+		<div className='home-container'>
+			<div className='activities-container'>
+       
+      </div>
 
-  );
+			<div className='cart-summary'>hello cart summary</div>
+		</div>
+	);
 };
 
 export default Home;
